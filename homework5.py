76ebd9addbfde7807360a56352cab52b7fbe061e
rt34kf <Shchepkina.mika@yandex.ru>
2024-07-03 21:10:33 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,6 @@
+immutable_var = 2, 10, 'apple', 'monday'
+print('Immutable tuple:' , immutable_var)
+#immutable_var[2] = 100  "не поддерживает обращение по элементам"
+mutable_list = [4, 7, 9, 'wednesday', 'lemon']
+mutable_list[2] = 15
+print('Mutable list:' , mutable_list)
